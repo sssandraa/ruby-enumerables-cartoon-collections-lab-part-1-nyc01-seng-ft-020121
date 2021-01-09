d7cd214043279ba23bc0fe98a691b355a09502f1
Sandra Wazaz <sandrawazaz@gmail.com>
2021-01-09 16:22:31 +0000

    Automatically backed up by Learn

--- practice.rb
+++ practice.rb
@@ -3,6 +3,7 @@ string = "Hello there, and how are you?"
 def reverse_each_word(string)
   string.split
   x = string.split
-  x.each {|word| word.reverse!}
+  x.each {|word| word.reverse!.to_s}
+
 end
 p reverse_each_word(string)
